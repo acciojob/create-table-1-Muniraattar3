348b6238b72344548c59e5d45723974fbab4484d
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2024-12-31 13:26:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  
+   const table= document.getElementById('sampleTable');
+
+	const newRow= table.insertRow(0);
+
+	const newCell0 = newRow.insertCell(0);
+	const newCell1 = newRow.insertCell(1);
+
+	newCell0.textContent = 'New Cell1';
+	newCell1.textContent = 'New Cell2';
   
 }
